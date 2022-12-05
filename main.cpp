a62e0781b9ad49a81f8b2fc7d3e8b98cfa8ab082
hupan <hupan008@qq.com>
2022-12-05 15:05:39 +0800

    hupan:添加方法

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include "GLAD/include/glad/glad.h"
 #include <GLFW/glfw3.h>
+bool fibon_elem(int pos, int &result)
+{
+    if(pos < 0 || pos >1024)
+    {result = 0; return false;}
+    int n_1 = 1, n_2 = 1;
+    result = 1;
+    for(int x = 3; x <= pos; ++x)
+    {result = n_1 + n_2;n_1 = n_2; n_2 = result;}
+    return true;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+    // 关闭应用程序
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GL_TRUE);
+    if(key == GLFW_KEY_A && action == GLFW_RELEASE) std::cout << "test A" << std::endl;
+    const GLubyte *name = glGetString(GL_VENDOR);           //返回负责当前OpenGL实现厂商的名字
+    const GLubyte *biaoshifu = glGetString(GL_RENDERER);    //返回一个渲染器标识符，通常是个硬件平台
+    const GLubyte *OpenGLVersion = glGetString(GL_VERSION); //返回当前OpenGL实现的版本号
+    printf("OpenGL实现厂商的名字：%s\n", name);
+    printf("渲染器标识符：%s\n", biaoshifu);
+    printf("OpenGL实现的版本号：%s\n", OpenGLVersion);
+}    
 
 int main(int argc, char const *argv[])
 {
@@ -18,12 +42,16 @@ int main(int argc, char const *argv[])
         return -1;
     }
     glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);  
+
     //把OpenGL的函数指针导入给GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
 
@@ -31,15 +59,13 @@ int main(int argc, char const *argv[])
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
     }
 
-    const GLubyte* name = glGetString(GL_VENDOR); //返回负责当前OpenGL实现厂商的名字
-    const GLubyte* biaoshifu = glGetString(GL_RENDERER); //返回一个渲染器标识符，通常是个硬件平台
-    const GLubyte* OpenGLVersion =glGetString(GL_VERSION); //返回当前OpenGL实现的版本号
-    printf("OpenGL实现厂商的名字：%s\n", name);
-    printf("渲染器标识符：%s\n", biaoshifu);
-    printf("OpenGL实现的版本号：%s\n",OpenGLVersion );
     glfwTerminate();
     return 0;
 }
