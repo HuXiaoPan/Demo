acff07b41207f9bb908149ece8bf9f7ef76f6675
hupan <hupan008@qq.com>
2023-03-03 15:22:13 +0800

    hupan:你好三角形

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 void main_test_LCM();
-void main_test_OPGL(int argc, char const *argv[]);
+int main_test_OPGL(int argc, char const *argv[]);
 int main(int argc, char const *argv[])
 {
     // main_test_LCM();
